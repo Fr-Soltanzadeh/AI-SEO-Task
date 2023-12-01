61752b7bb7f37d3837d3e5b6c19c6ee1d030f21b
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-12-01 22:28:36 +0330

    debug

--- AI-keyword-app/app/Controllers/Http/KeywordsController.ts
+++ AI-keyword-app/app/Controllers/Http/KeywordsController.ts
@@ -1,23 +1,26 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 import OpenAI from "openai";
-// import Env from '@ioc:Adonis/Core/Env'
 
-// const OPENAI_SECRET_KEY: string = Env.get('OPENAI_SECRET_KEY')
-
+const OPENAI_API_KEY= process.env.OPENAI_API_KEY
 
 export default class KeywordsController {
     public async create({request, response}: HttpContextContract) {
         const topic = request.input("topic");
         const keyword_number = request.input("keyword_number", 10)
         const content = `Identify ${keyword_number} SEO keywords related to ${topic}`
-        try{
-            const openai = new OpenAI();
+        if (!OPENAI_API_KEY) {
+            return response.status(500).json({ message: 'OpenAI API key is missing.' });
+        }
+        try{  
+            const openai = new OpenAI({apiKey: OPENAI_API_KEY});
             const result = await openai.chat.completions.create({
+                model: "gpt-3.5-turbo",
                 messages: [{ role: 'user', content: content }],
-                model: 'gpt-4',
-              }).withResponse();
-            return response.json(result)
+              });
+            console.log(result)
+            const seoKeywords = result.choices[0].message.content;
+            return response.status(200).json(seoKeywords)
         }catch(error){
             console.error(error)
             return response.status(500).json({ message: "AI service is not available now!" })
